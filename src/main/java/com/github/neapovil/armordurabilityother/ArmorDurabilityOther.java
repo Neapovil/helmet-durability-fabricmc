9c383d6b3046895815bcaf2477d6031c729b982c
William <nearata@protonmail.com>
2022-09-21 14:55:43 +0200

    init project

---
+++ src/main/java/com/github/neapovil/armordurabilityother/ArmorDurabilityOther.java
@@ -0,0 +1,11 @@
+package com.github.neapovil.armordurabilityother;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmorDurabilityOther implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}

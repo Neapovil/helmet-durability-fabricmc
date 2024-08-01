dbab4de4464b3c9a07d2d92a5897751f22736227
William <nearata@protonmail.com>
2024-08-01 21:05:40 +0200

    improvements

--- src/main/java/com/github/neapovil/helmetdurability/mixin/EntityRendererMixin.java
+++ src/main/java/com/github/neapovil/helmetdurability/mixin/EntityRendererMixin.java
@@ -1,6 +1,7 @@
 package com.github.neapovil.helmetdurability.mixin;
 
 import com.github.neapovil.helmetdurability.HelmetDurability;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.util.Colors;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
@@ -10,9 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -23,27 +22,21 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 
 @Mixin(EntityRenderer.class)
-public class EntityRendererMixin
+public abstract class EntityRendererMixin<T extends Entity>
 {
     @Shadow
     @Final
     private TextRenderer textRenderer;
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V"),
-            method = "renderLabelIfPresent")
-    protected void renderLabelIfPresent(Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float tickDelta, CallbackInfo ci)
+    @Inject(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I", ordinal = 0))
+    protected void renderLabelIfPresent(T entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float tickDelta, CallbackInfo ci, @Local boolean bl, @Local Matrix4f matrix4f, @Local(ordinal = 1) int j)
     {
         if (!HelmetDurability.INSTANCE.config().enabled)
         {
             return;
         }
 
-        if (!(entity instanceof PlayerEntity player))
-        {
-            return;
-        }
-
-        if (entity instanceof ClientPlayerEntity)
+        if (!(entity instanceof PlayerEntity playerentity))
         {
             return;
         }
@@ -53,24 +46,22 @@ public class EntityRendererMixin
             return;
         }
 
-        final ItemStack helmet = player.getInventory().getArmorStack(3);
+        final ItemStack helmet = playerentity.getInventory().getArmorStack(3);
         final int damage = helmet.getMaxDamage() - helmet.getDamage();
 
-        final Text text1 = Text.literal("" + damage).setStyle(Style.EMPTY.withColor(helmet.getItemBarColor()).withBold(true));
-
-        final MinecraftClient client = MinecraftClient.getInstance();
-        float h = (float) -this.textRenderer.getWidth(text1) / 2;
-        float g = client.options.getTextBackgroundOpacity(0.25f);
-        final int j = (int) (g * 255.0f) << 24;
-        final int y = -10;
-        final boolean visible = !player.isSneaky();
-        final Matrix4f matrix4f = matrices.peek().getPositionMatrix();
-
-        this.textRenderer.draw(text1, h, (float) y, 0x20FFFFFF, false, matrix4f, vertexConsumers, visible ? TextRenderer.TextLayerType.SEE_THROUGH : TextRenderer.TextLayerType.NORMAL, j, light);
-
-        if (visible)
+        if (damage < 1)
         {
-            this.textRenderer.draw(text1, h, (float) y, Colors.WHITE, false, matrix4f, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0, light);
+            return;
+        }
+
+        final Text text1 = Text.literal("" + damage).setStyle(Style.EMPTY.withColor(helmet.getItemBarColor()).withBold(true));
+        float g = (float)(-this.textRenderer.getWidth(text1) / 2);
+
+        this.textRenderer.draw(text1, g, (float) -10, 0x20FFFFFF, false, matrix4f, vertexConsumers, bl ? TextRenderer.TextLayerType.SEE_THROUGH : TextRenderer.TextLayerType.NORMAL, j, light);
+
+        if (bl)
+        {
+            this.textRenderer.draw(text1, g, (float) -10, Colors.WHITE, false, matrix4f, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0, light);
         }
     }
 }
